e870b45fd6b8a70c2d495f50203c0b216d157c3c
MikeHuijgen <mikeh162001@hotmail.com>
2026-02-11 15:17:07 +0100

    Update InputHandler.cs

--- Proef-proeve/Assets/Scripts/Framework/InputHandler/InputHandler.cs
+++ Proef-proeve/Assets/Scripts/Framework/InputHandler/InputHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerInput))]
 public class InputHandler : MonoBehaviour
 {
     public static InputHandler Instance;
