4e11ac2ce83a9bea3463c6f3b00260609ba8f99c
MikeHuijgen <mikeh162001@hotmail.com>
2026-02-11 15:07:06 +0100

    Added the basic state logic for the base state

--- Proef-proeve/Assets/Scripts/Framework/StateMachine/StateMachine.cs
+++ Proef-proeve/Assets/Scripts/Framework/StateMachine/StateMachine.cs
@@ -3,4 +3,11 @@ using UnityEngine;
 public class StateMachine : MonoBehaviour
 {
     private BaseState _currentState;
+
+    private void SwitchState(BaseState newState)
+    {
+        _currentState.StateExit();
+        _currentState = newState;
+        _currentState.StateEnter();
+    }
 }
--- Proef-proeve/Assets/Scripts/Framework/States/BaseState.cs
+++ Proef-proeve/Assets/Scripts/Framework/States/BaseState.cs
@@ -1,6 +1,13 @@
 using UnityEngine;
 
-public class BaseState : MonoBehaviour
+public abstract class BaseState : MonoBehaviour
 {
-    
+    private bool _canBeInterrupted;
+
+    public abstract void StateEnter();
+    public abstract void StateUpdate();
+    public abstract void StateExit();
+    public virtual void OnStateInterrupted(){}
+
+    public bool CanBeInterrupted => _canBeInterrupted;
 }
