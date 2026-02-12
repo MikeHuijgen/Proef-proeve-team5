fb8ae2040b0c1ec6b664a66e264d38594499b810
Gerben-Prins <114744911+Gerben-Prins@users.noreply.github.com>
2026-02-12 14:01:54 +0100

    Started Working on Movement

---
+++ Proef-proeve/AGENTS.md
@@ -0,0 +1,14 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: Proef-proeve
+- Unity version: Unity 6000.3.7f1
+- Active scene:
+  - Name: PlanetMovement
+  - Tags:
+    - Untagged, Respawn, Finish, EditorOnly, MainCamera, Player, GameController
+  - Layers:
+    - Default, TransparentFX, Ignore Raycast, Water, UI, Ground
+- Active game object:
+  - Name: Player
+  - Tag: Untagged
+  - Layer: Default
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Proef-proeve/Assets/Scripts/Framework/Player/PlayerMovement.cs
+++ Proef-proeve/Assets/Scripts/Framework/Player/PlayerMovement.cs
@@ -1,6 +1,21 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : PlayerDetection
 {
-    
+    [Header("Movement Settings")]
+    [SerializeField] private float MoveSpeed = 5f;
+    private Rigidbody _rb;
+
+        private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        // Optional: freeze rotation if you don't want physics rotation
+        _rb.freezeRotation = true;
+    }
+
+    void Update()
+    {
+        Debug.Log(InputHandler.Instance.GetMoveValue());
+    }
 }
