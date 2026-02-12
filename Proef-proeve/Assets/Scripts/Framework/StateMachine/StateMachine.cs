0e63102b7c33e66170b11db338a19ae43735963b
MikeHuijgen <mikeh162001@hotmail.com>
2026-02-12 10:02:01 +0100

    Added the class for the IntentToState

--- Proef-proeve/Assets/Scripts/Framework/StateMachine/StateMachine.cs
+++ Proef-proeve/Assets/Scripts/Framework/StateMachine/StateMachine.cs
@@ -1,13 +1,31 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class StateMachine : MonoBehaviour
 {
+    [SerializeField] private IntentToState[] intentToStates;
     private BaseState _currentState;
+    private Dictionary<int, BaseState> _intentToStateDictionary;
+
+    private void OnEnable()
+    {
+        //Hier moeten we op de inputhandler eventhandler aanmelden
+    }
+
+    void OnDisable()
+    {
+        //Hier moeten we op de inputhandler eventhandler afmelden 
+    }
 
     private void SwitchState(BaseState newState)
     {
         _currentState.StateExit();
         _currentState = newState;
         _currentState.StateEnter();
+    }
+
+    private void OnNewIntent()
+    {
+        
     }
 }
