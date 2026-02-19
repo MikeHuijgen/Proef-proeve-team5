433f5298f9a1577ec0a83508aad551e1e21b8965
JonasDriessen0 <114576261+JonasDriessen0@users.noreply.github.com>
2026-02-19 13:34:33 +0100

    Improved movement and added jumping

--- Proef-proeve/Assets/Scripts/Framework/Player_Movement/PlayerJump.cs
+++ Proef-proeve/Assets/Scripts/Framework/Player_Movement/PlayerJump.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerJump : MovementComponent
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float _jumpHeight = 6f;
+    [SerializeField] private InputAction jumpAction;
+
+    private PlayerGravity _gravity;
+    private GroundCheck _groundCheck;
+
+    private void OnEnable() => jumpAction.Enable();
+    private void OnDisable() => jumpAction.Disable();
+
+    private void Start()
     {
-        
+        _gravity = GetComponent<PlayerGravity>();
+        _groundCheck = GetComponent<GroundCheck>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (_gravity == null || _groundCheck == null)
+            return;
+
+        if (_groundCheck.IsGrounded && jumpAction.WasPressedThisFrame())
+        {
+            float jumpSpeed = Mathf.Sqrt(2f * _gravity.GravityStrength * _jumpHeight);
+            _gravity.Jump(jumpSpeed);
+        }
     }
 }
