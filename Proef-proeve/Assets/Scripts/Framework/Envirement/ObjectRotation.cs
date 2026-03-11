9e145d8bd78d15c1add5b8051472f11dfbc09985
Gerben-Prins <114744911+Gerben-Prins@users.noreply.github.com>
2026-03-11 09:43:36 +0100

    Removed Comments

--- Proef-proeve/Assets/Scripts/Framework/Envirement/ObjectRotation.cs
+++ Proef-proeve/Assets/Scripts/Framework/Envirement/ObjectRotation.cs
@@ -2,11 +2,10 @@ using UnityEngine;
 
 public class ObjectRotation : MonoBehaviour
 {
-    [SerializeField] private float rotateSpeed = 50f; // Rotation speed (degrees per second)
+    [SerializeField] private float rotateSpeed = 50f;
 
     private void Update()
     {
-        // Rotate the object around its Y-axis at the specified speed
         transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
     }
 }
