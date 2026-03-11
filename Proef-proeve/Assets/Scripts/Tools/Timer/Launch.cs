d6440567b5d2db80df9b67ac8d799bdca0e95a96
keithko <keith.kolakowski@stichtingheldeninit.nl>
2026-03-11 11:50:24 +0100

    try to solution
    tryed the addit and chanche the gravity and put it off but did not found a solution

--- Proef-proeve/Assets/Scripts/Tools/Timer/Launch.cs
+++ Proef-proeve/Assets/Scripts/Tools/Timer/Launch.cs
@@ -1,15 +1,15 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Launch : MonoBehaviour
 {
     public static event Action<StateIntentData> OnLaunch;
     public static event Action OnLanded;
-    PlayerGravity _gravity;
-    MovementData _movementData;
-    [SerializeField] private Transform _startLocation;
 
     [SerializeField] private StateIntentData travelIntent;
+
+    [SerializeField] private Transform _startLocation;
     [SerializeField] private Transform _endLocation;
     [SerializeField]private Transform _nextPlanet;
 
@@ -19,19 +19,23 @@ public class Launch : MonoBehaviour
     [SerializeField] private float _speed;
     [SerializeField] private float _travelTime;
 
+    private PlayerGravity _gravity;
+    private MovementData _movementData;
     private CapsuleCollider _capsuleCollider;
+    private PlayerMovement _playerMovement;
 
-    private float _elapsedTime;
+
     private float _planetDistance;
     private float _journey;
 
     private bool _isTravel;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
-    {
+    { 
         _movementData = _player.GetComponent<MovementData>();
         _gravity = _player.GetComponent<PlayerGravity>();
         _capsuleCollider = GetComponent<CapsuleCollider>();
+        _playerMovement = _player.GetComponent<PlayerMovement>();
 
         _planetDistance = Vector3.Distance(_startLocation.position, _endLocation.position);
     }
@@ -39,10 +43,11 @@ public class Launch : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        
     // Check if travel is true, and if it is Gameobject start traveling.
         if (_isTravel)
         {
-            _elapsedTime = (Time.time - _travelTime) * _speed;
+            _journey += Time.deltaTime * _speed / _planetDistance;
             Traveling();
         }
     }
@@ -50,14 +55,15 @@ public class Launch : MonoBehaviour
     private void Traveling()
     {
         // check the distandce between planets and devide it with the time it will take to get there.
-        _journey = _elapsedTime / _planetDistance;
         transform.rotation = Quaternion.Lerp(Quaternion.identity, Quaternion.Euler(_landRotation, 0, 0), _journey);
         transform.position = Vector3.Lerp(_startLocation.position, _endLocation.position, _journey);
         // reset the journey if it is done.
         if (_journey >= 1f)
         {
+            _playerMovement.enabled = true;
             _gravity.enabled = true;
             _isTravel = false;
+            _player.transform.SetParent(null);
         }
     }
 
@@ -74,6 +80,7 @@ public class Launch : MonoBehaviour
             _isTravel =  true;
             _player.transform.SetParent(transform);
             _capsuleCollider.enabled = false;
+            _playerMovement.enabled = false;
         }
     }
 }
