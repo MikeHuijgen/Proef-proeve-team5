72dcb8c129a690b34e3d280fc8a15953676c2ce0
Gerben-Prins <114744911+Gerben-Prins@users.noreply.github.com>
2026-03-12 13:16:46 +0100

    Working Shock

--- Proef-proeve/Assets/Scripts/Framework/ShockThruster/ShockThruster.cs
+++ Proef-proeve/Assets/Scripts/Framework/ShockThruster/ShockThruster.cs
@@ -4,6 +4,7 @@ public class ShockThruster : MonoBehaviour
 {
     [SerializeField] private float shockInterval;
     [SerializeField] private BoxCollider shockBoxCollider;
+    [SerializeField] private GameObject particle;
     private CountdownTimer _shockTimer;
 
     private void Awake()
@@ -11,6 +12,7 @@ public class ShockThruster : MonoBehaviour
         _shockTimer = new CountdownTimer(shockInterval);
         _shockTimer.StartTimer();
         DisableBoxCollider();
+        DisableParticle();
     }
 
     private void Update()
@@ -20,14 +22,17 @@ public class ShockThruster : MonoBehaviour
         if (!_shockTimer.IsTimerDone) return;
 
         _shockTimer.StopTimer();
-
+        EnableParticle();
         EnableBoxCollider();
 
         Invoke("DisableBoxCollider", 1f);
+        Invoke("DisableParticle", 1f);
 
         _shockTimer.StartTimer();
     }
 
     private void EnableBoxCollider() => shockBoxCollider.enabled = true;
     private void DisableBoxCollider() => shockBoxCollider.enabled = false;
+    private void EnableParticle() => particle.SetActive(true);
+    private void DisableParticle() => particle.SetActive(false);
 }
