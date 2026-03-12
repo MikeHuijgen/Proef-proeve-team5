5de0db3bb9d5d7c96a5c389a0a516051d2c843b3
MikeHuijgen <mikeh162001@hotmail.com>
2026-03-12 11:25:06 +0100

    Fixed that sometimes the rocket is not  above the ground

--- Proef-proeve/Assets/Scripts/Tools/Timer/Launch.cs
+++ Proef-proeve/Assets/Scripts/Tools/Timer/Launch.cs
@@ -63,8 +63,9 @@ public class Launch : MonoBehaviour
         {
             _isTravel = false;
             _playerTransform.parent = null;
-            var normal = (transform.position - endLocation.position).normalized;
-            transform.position += normal * 2f;
+            Vector3 normal = (transform.position - nextPlanet.position).normalized;
+            transform.position = endLocation.position + normal * 2f;
+
 
             OnLanded?.Invoke();
         }
